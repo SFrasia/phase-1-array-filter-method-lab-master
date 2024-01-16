e579b5d42922063e31a286025a3fe4b2d3f98580
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-01-16 13:19:20 +0300

    Changes

---
+++ index.js
@@ -0,0 +1,26 @@
+// Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+const findMatching = drivers.find(function (element) {
+  return element === "Bobby";
+});
+
+const findSusan = drivers.find(function (element) {
+  return element === "Susan";
+});
+
+const Alldrivers = drivers(drivers.length);
+
+const firstDriver = drivers.find(function (driver) {
+  return driver.startsWith("Sa");
+});
+
+const matchDriver = drivers.find(function (driver) {
+  return driver.startsWith("y");
+});
+
+const middleEndDiver = drivers.find(function (driver) {
+  return driver.startsWith("mm");
+});
+
+const result = drivers.find(({ name }) => name === "Bobby");
